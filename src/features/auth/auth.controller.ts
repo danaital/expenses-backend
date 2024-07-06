043e9fa7387cd1a3a938e8b84bd12fa3b6dbdd4a
taldanai1995 <taldanai@icloud.com>
2024-07-06 21:10:15 +0300

    add todos

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -5,6 +5,15 @@ import { Body, Controller, Post } from '@nestjs/common';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  // TODO: add i18n translation for error messages, success messages, and other messages and labels
+  // TODO: add jwt
+  // TODO: add salt rounds to config
+  // TODO: add validation
+  // TODO: add logging
+  // TODO: add error handling
+  // TODO: add unit tests
+  // TODO: add e2e tests
+  // TODO: add DTOs for request and response objects
   @Post('login')
   login(@Body() body: any) {
     const { username, password } = body;
